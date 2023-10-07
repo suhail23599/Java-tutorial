f56cc608f6b16f7265064c0eaa739d9b0a2f183c
suhail23599 <suhail23599@gmail.com>
2023-10-07 10:55:09 +0530

    initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        Scanner s = new Scanner(System.in);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.BYTES);
+    }
+}
