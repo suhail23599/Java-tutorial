3e22fd2f6e0e594d9b93d62c0b494695554aa35d
suhail23599 <suhail23599@gmail.com>
2023-10-11 23:41:24 +0700

    format printing

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,32 @@ import java.lang.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        byte b = 10;
-        short s = 10;
-        int i = 10;
-        long l = 10;
-        float f = 4.2f;
-        char c = '2';
-        double d = 10;
-        boolean bl = false;
+        int a = 10;
+        int b = 20;
+        System.out.println(a + "" + b);
 
-//       b = s; not possible
-        s = b;
-        f = i;
-        i = c;
-//        c = b; not possible, character we can assign only interger/character, they are not compatible
-        System.out.println(i);
+        System.out.println("sum" + a + b ); // sum1020
 
+        System.out.println(a + b + "sum" ); // 30sum
+
+        // formatted output
+        float f = 2.4f;
+        char c = 'a';
+        int num = 10;
+        System.out.printf("hello %f \n", f);
+
+        System.out.printf("%1$d %1$d \n", num); // 1$ -> first argument
+
+        int num1 = 5;
+        System.out.printf("%5d \n", 5); // width -5
+        System.out.printf("%05d \n", 5); // flag - 0, fill the empty space w zero
+
+        int neg = -10;
+        System.out.printf("%(5d \n", neg); // flag - (, if num is neg it will be enclosed in (), for positive no ()
+
+        System.out.printf("%+5d \n", neg); // flag - +, it will print the sign of number
+
+        float f_num = 12.453f;
+        System.out.printf("%6.3f \n", f_num); // precision .2 places,only 5 digits
     }
 }
