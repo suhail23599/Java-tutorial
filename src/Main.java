a0603a6e36afccfaecd974fdae4fc9f9d1c1820d
suhail23599 <suhail23599@gmail.com>
2023-10-11 22:53:33 +0700

    widening/narrowing

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,21 @@ import java.lang.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-//        int x = 0b1010;
-//        int y = 0b0110;
-//        int z = x & y;
-//        System.out.println(z);
-//
-//        int orOp = x | y;
-//        System.out.println(orOp);
-//
-//        int xOr = x ^ y;
-//        System.out.println(xOr);
-//
-//        int a = 10;
-//        int leftShift = a<<1; // double the value by 2^1
-//        System.out.println(leftShift);
-//
-//        int rightShift = a>>1; // divide the value by 2^1
-//        System.out.println(rightShift);
-//
-//        System.out.println(String.format("%32s", Integer.toBinaryString(a)));
+        byte b = 10;
+        short s = 10;
+        int i = 10;
+        long l = 10;
+        float f = 4.2f;
+        char c = '2';
+        double d = 10;
+        boolean bl = false;
 
-        // swap 2 variables without third variable
-        int var1 = 12;
-        int var2 = 10;
-        var1 = var1 ^ var2; // result will not be maximum of these 2 numbers
-        var2 = var1 ^ var2;
-        var1 = var1 ^ var2;
-        System.out.println(var1);
-        System.out.println(var2);
+//       b = s; not possible
+        s = b;
+        f = i;
+        i = c;
+//        c = b; not possible, character we can assign only interger/character, they are not compatible
+        System.out.println(i);
+
     }
 }
